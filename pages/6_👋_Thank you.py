e1866c3e763885aa346ca068f469d0d1f009647c
Vincent Tu <tuvincent0106@gmail.com>
2022-12-29 21:41:57 -0800

    finished thank you note; added requirements.txt

--- pages/6_👋_Thank you.py
+++ pages/6_👋_Thank you.py
@@ -1,10 +1,12 @@
 # Imports.
+import numpy as np
+from PIL import Image
 import streamlit as st
 
 # Page config.
 st.set_page_config(
-    page_title="The Data Science Process",
-    page_icon="🧪",
+    page_title="Thank you!",
+    page_icon="🙏",
     layout="wide",
     initial_sidebar_state="expanded",
     menu_items={
@@ -13,24 +15,26 @@ st.set_page_config(
 )
 
 # Title.
-st.title("The Data Science Process")
-
-# Table of contents.
+st.title("Thank You!")
 
 st.markdown("""
-**Table of Contents**: <br>
+    I began this project wanting to pursue a more comprehensive take on projects. I wanted a series 
+    of projects that would tackle every aspect of the data science pipeline. At the time (2021), I only had
+    a bit of predictive modeling and data preprocessing under my belt. I wanted to explore more into this data science realm.
+    This lengthy project did just that. It taught me an awful lot, both technically and non-technically speaking.
+    The next steps for me is starting a deep learning project! I've done plenty of deep learning in the past, but 
+    I don't think any of them were as thorough as this one. I'll have to mirror this project's depth! 
 
-<ul>
-    <li>1. <a href=\"#1-introduction\">Introduction</a></li>
-    <li>2. <a href=\"#2-cleaning\">Cleaning</a></li>
-    <li>3. <a href=\"#3-eda\">Exploratory Data Analysis (EDA)</a>
-        <ul>
-            <li>3.1. <a href=\"#3-1-a-univariate-look\">A Univariate Look</a></li>
-            <li>3.2. <a href=\"#3-2-a-multivariate-dive\">A Multivariate Dive</a></li>
-            <li>3.3. <a href=\"#3-3-a-free-form-exploration\">A Free-form Exploration</a></li>
-        </ul>
-    </li>
-    <li>4. <a href=\"#4-conclusion\">Conclusion</a></li>
-    <li>5. <a href=\"#5-author-info\">Author Info</a></li>
-</ul>
-""", unsafe_allow_html=True)
+    This page is a short thank you note and so I'd like to thank all the references in both
+    my notebook and in `resources.md`. Without those guides, articles, and demos, I wouldn't have been
+    able to do this project. I'd also like to express some appreciation for these fantastic libraries!
+    And by libraries I mean all the tools I've used up to this point. Though I've ran into countless
+    bugs, these libraries were thorough, easy to understand, and had great documentation. And thank you 
+    [Catherine](https://github.com/crasgaitis) for your help with the visuals and banners. You made this 
+    project all the more memorable. ❤️ 
+
+    And thank *you* for reading this! I hope this project was an absolute joy for you to look through.
+""")
+
+image = np.asarray(Image.open("./img/waving_hand.png"))
+st.image(image)
